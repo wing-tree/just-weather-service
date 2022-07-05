89ca3a14f36c7445c9338e6a58e7774f308d643d
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-05 18:51:59 +0900

    refactor : Apply clearAndInsert and dt functions

--- data/src/main/java/com/wing/tree/just/weather/service/data/datasource/local/OpenWeatherDataSource.kt
+++ data/src/main/java/com/wing/tree/just/weather/service/data/datasource/local/OpenWeatherDataSource.kt
@@ -3,7 +3,7 @@ package com.wing.tree.just.weather.service.data.datasource.local
 import com.wing.tree.just.weather.service.data.entity.Forecast
 
 interface OpenWeatherDataSource {
-    suspend fun clear()
+    suspend fun clearAndInsert(forecast: Forecast)
+    suspend fun dt(): Long?
     suspend fun forecast(): Forecast?
-    suspend fun insert(forecast: Forecast)
 }
--- data/src/main/java/com/wing/tree/just/weather/service/data/datasource/local/OpenWeatherDataSourceImpl.kt
+++ data/src/main/java/com/wing/tree/just/weather/service/data/datasource/local/OpenWeatherDataSourceImpl.kt
@@ -7,15 +7,15 @@ import javax.inject.Inject
 class OpenWeatherDataSourceImpl @Inject constructor(database: Database) : OpenWeatherDataSource {
     private val forecastDao = database.forecastDao()
 
-    override suspend fun clear() {
-        forecastDao.clear()
+    override suspend fun clearAndInsert(forecast: Forecast) {
+        forecastDao.clearAndInsert(forecast)
     }
 
-    override suspend fun insert(forecast: Forecast) {
-        forecastDao.insert(forecast)
+    override suspend fun dt(): Long? {
+        return forecastDao.dt()
     }
 
     override suspend fun forecast(): Forecast? {
-        return forecastDao.get()
+        return forecastDao.forecast()
     }
 }
