c5411330f38b70320af54a58f8586d145e366fa6
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-05 18:00:12 +0900

    feat : Declare hilt di for LocalOpenWeatherDataSource

--- data/src/main/java/com/wing/tree/just/weather/service/data/di/DataSourceModule.kt
+++ data/src/main/java/com/wing/tree/just/weather/service/data/di/DataSourceModule.kt
@@ -2,8 +2,10 @@ package com.wing.tree.just.weather.service.data.di
 
 import com.wing.tree.just.weather.service.data.datasource.local.LocationDataSource
 import com.wing.tree.just.weather.service.data.datasource.local.LocationDataSourceImpl
-import com.wing.tree.just.weather.service.data.datasource.remote.OpenWeatherDataSource
-import com.wing.tree.just.weather.service.data.datasource.remote.OpenWeatherDataSourceImpl
+import com.wing.tree.just.weather.service.data.datasource.local.OpenWeatherDataSource as LocalOpenWeatherDataSource
+import com.wing.tree.just.weather.service.data.datasource.local.OpenWeatherDataSourceImpl as LocalOpenWeatherDataSourceImpl
+import com.wing.tree.just.weather.service.data.datasource.remote.OpenWeatherDataSource as RemoteOpenWeatherDataSource
+import com.wing.tree.just.weather.service.data.datasource.remote.OpenWeatherDataSourceImpl as RemoteOpenWeatherDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -19,5 +21,9 @@ internal abstract class DataSourceModule {
 
     @Binds
     @Singleton
-    abstract fun bindsOpenWeatherDataSource(dataSource: OpenWeatherDataSourceImpl): OpenWeatherDataSource
+    abstract fun bindsLocalOpenWeatherDataSource(dataSource: LocalOpenWeatherDataSourceImpl): LocalOpenWeatherDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindsRemoteOpenWeatherDataSource(dataSource: RemoteOpenWeatherDataSourceImpl): RemoteOpenWeatherDataSource
 }
