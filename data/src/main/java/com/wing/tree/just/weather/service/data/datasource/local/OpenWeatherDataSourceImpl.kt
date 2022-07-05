403923db18e94793e1c0eed5d29ceaf24f1c56bc
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-05 17:53:47 +0900

    feat : Declare hilt di for Database

---
+++ data/src/main/java/com/wing/tree/just/weather/service/data/datasource/local/OpenWeatherDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package com.wing.tree.just.weather.service.data.datasource.local
+
+import com.wing.tree.just.weather.service.data.database.Database
+import com.wing.tree.just.weather.service.data.entity.Forecast
+import javax.inject.Inject
+
+class OpenWeatherDataSourceImpl @Inject constructor(database: Database) : OpenWeatherDataSource {
+    private val forecastDao = database.forecastDao()
+
+    override suspend fun clear() {
+        forecastDao.clear()
+    }
+
+    override suspend fun insert(forecast: Forecast) {
+        forecastDao.insert(forecast)
+    }
+
+    override suspend fun forecast(): Forecast {
+        return forecastDao.get()
+    }
+}
