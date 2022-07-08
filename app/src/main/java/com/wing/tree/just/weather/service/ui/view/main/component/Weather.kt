568203af400343f0a220cd216f200aabd7b39f6c
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-07-08 09:12:01 +0900

    feat : Implement Weather component

--- app/src/main/java/com/wing/tree/just/weather/service/ui/view/main/component/Weather.kt
+++ app/src/main/java/com/wing/tree/just/weather/service/ui/view/main/component/Weather.kt
@@ -1,29 +1,69 @@
 package com.wing.tree.just.weather.service.ui.view.main.component
 
+import android.location.Address
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Menu
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import com.wing.tree.just.weather.service.constant.DEGREE_SIGN
+import com.wing.tree.just.weather.service.constant.icons
+import com.wing.tree.just.weather.service.domain.constant.EMPTY
 import com.wing.tree.just.weather.service.domain.model.local.openweather.Weather
+import com.wing.tree.just.weather.service.ui.view.main.WeatherState
+import kotlin.math.roundToInt
 
 @Composable
 fun Weather(
     modifier: Modifier,
+    address: Address?,
+    uiState: WeatherState
+) {
+    when(uiState) {
+        is WeatherState.Loading -> Unit
+        is WeatherState.Content -> uiState.weather?.let { weather ->
+            WeatherContent(
+                modifier = modifier,
+                address = address,
+                weather = weather
+            )
+        }
+        is WeatherState.Error -> Unit
+    }
+}
+
+@Composable
+fun WeatherContent(
+    modifier: Modifier,
+    address: Address?,
     weather: Weather
 ) {
+    val tempText = "${weather.temp.roundToInt()}$DEGREE_SIGN"
+    val locality = address?.locality
+    val subLocality = address?.subLocality
+
     Row(modifier = modifier) {
-        Column(modifier = Modifier) {
-            IconButton(onClick = {  }) {
-                Icon(Icons.Rounded.Menu, null)
+        Column(
+            modifier = Modifier.fillMaxWidth(0.5F),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = tempText)
+            Text(text = locality ?: subLocality ?: EMPTY)
+        }
+
+        Column(
+            modifier = Modifier.fillMaxWidth(0.5F),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val painter = icons[weather.icon]?.let {
+                painterResource(id = it)
             }
 
-            Text(text = "${weather.temp}$DEGREE_SIGN")
+            painter?.let { Image(painter = it, contentDescription = null) }
         }
     }
 }
